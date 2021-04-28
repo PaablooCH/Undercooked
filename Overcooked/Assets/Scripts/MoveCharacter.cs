890fcf3f23c552d544d99ffc5833420a7e101368
Victorsitu <victor.martinez.m@estudiantat.upc.edu>
2021-04-28 21:19:54 +0200

    Fixed Collider

--- Overcooked/Assets/Scripts/MoveCharacter.cs
+++ Overcooked/Assets/Scripts/MoveCharacter.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MoveCharacter : MonoBehaviour
 {
     public float speed = 3.0f;
+    bool holding = false;
+    public Transform Dest;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +24,32 @@ public class MoveCharacter : MonoBehaviour
             transform.Translate(0.0f, 0.0f, -speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.UpArrow))
             transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerStay(Collider obj)
+    {
+        if (obj.tag == "Food") 
+        { 
+            if (Input.GetKey(KeyCode.Space) && !holding)
+            {
+                obj.GetComponent<BoxCollider>().enabled = false;
+                obj.GetComponent<BoxCollider>().enabled = false;
+                obj.GetComponent<Rigidbody>().useGravity = false;
+                obj.transform.position = Dest.position;
+                obj.transform.parent = GameObject.Find("ToPickUp").transform;
+                obj.GetComponent<updateFoodPosition>().changeState();
+                holding = true;
+            }
+            if (Input.GetKey(KeyCode.Space) && holding)
+            {
+                obj.GetComponent<BoxCollider>().enabled = true;
+                obj.GetComponent<BoxCollider>().enabled = true;
+                obj.transform.parent = null;
+                obj.GetComponent<Rigidbody>().useGravity = true;
+                obj.GetComponent<updateFoodPosition>().changeState();
+                holding = false;
+            }
+        }
     }
 
 }
