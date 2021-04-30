2499cc1a125e9cb33f7b844f32506e50ef318f09
Victorsitu <victor.martinez.m@estudiantat.upc.edu>
2021-04-30 17:57:36 +0200

    PickUp & LeaveObject Done

--- Overcooked/Assets/Scripts/LeaveFood.cs
+++ Overcooked/Assets/Scripts/LeaveFood.cs
@@ -6,32 +6,49 @@ public class LeaveFood : MonoBehaviour
 {
     public Transform Dest;
     bool onTable;
+    GameObject food;
+    float cont;
 
     // Start is called before the first frame update
     void Start()
     {
         onTable = false;
+        cont = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cont += Time.deltaTime;
     }
 
     private void OnTriggerStay(Collider obj)
     {
-        if(obj.tag == "Furniture")
+        if (obj.tag == "Player")
         {
-            if (Input.GetKey(KeyCode.Space) && !onTable)
+            if (Input.GetKey(KeyCode.Space) && onTable && cont >= 1)
             {
-                this.GetComponent<BoxCollider>().enabled = true;
-                this.GetComponent<Rigidbody>().useGravity = true;
-                this.transform.position = Dest.position;
-                this.transform.parent = GameObject.Find("Leave").transform;
-                onTable = true;
-                //this.GetComponent<pickUpFood>().changeState();
+                onTable = false;
+                food = null;
+                cont = 0;
             }
+            //Debug.Log("Soy Player");
+        }
+        else if (obj.tag == "Food")
+        {
+            if (Input.GetKey(KeyCode.Space) && !onTable && cont >= 2)
+            {
+                //obj.GetComponent<BoxCollider>().enabled = true;
+                obj.GetComponent<Rigidbody>().useGravity = true;
+                obj.transform.position = Dest.position;
+                obj.transform.parent = GameObject.Find("Leave").transform;
+                onTable = true;
+                obj.gameObject.GetComponent<pickUpFood>().changeState();
+                obj.gameObject.GetComponent<pickUpFood>().resetCont();
+                food = obj.gameObject;
+                cont = 0;
+            }
+            //Debug.Log("Soy Comida");
         }
     }
 
--- Overcooked/Assets/Scripts/pickUpFood.cs
+++ Overcooked/Assets/Scripts/pickUpFood.cs
@@ -6,6 +6,7 @@ public class pickUpFood : MonoBehaviour
 {
     public Transform Dest;
     bool holded;
+    float cont = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,22 +16,24 @@ public class pickUpFood : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        cont += Time.deltaTime;
+        Debug.Log(cont);
     }
 
     private void OnTriggerStay(Collider obj)
     {
         if (obj.tag == "Player")
         {
-            if (Input.GetKey(KeyCode.Space) && !holded)
+            if (Input.GetKey(KeyCode.Space) && !holded && cont >= 1)
             {
-                this.GetComponent<BoxCollider>().enabled = false;
+                //this.GetComponent<BoxCollider>().enabled = false;
                 this.GetComponent<Rigidbody>().useGravity = false;
                 this.transform.position = Dest.position;
                 this.transform.parent = GameObject.Find("ToPickUp").transform;
                 holded = true;
                 //this.GetComponent<LeaveFood>().changeState();
             }
+            //Debug.Log("Soy Player");
         }
     }
 
@@ -38,5 +41,10 @@ public class pickUpFood : MonoBehaviour
     {
         if (holded) holded = false;
         else holded = true;
+    }
+
+    public void resetCont()
+    {
+        cont = 0;
     }
 }
