f283b0774fd9c23c1aeb1575914c009d0bc45a77
Pablo Cebollada Hernández <pablo_ch@MacBook-Pro-de-Pablo.local>
2021-05-15 20:59:26 +0200

    moveCharacter updated

--- Overcooked/Assets/Scripts/MoveCharacter.cs
+++ Overcooked/Assets/Scripts/MoveCharacter.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float playerSpeed = 3.0f;
     bool holding;
     public Transform Dest;
     public GameObject food;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
     // Start is called before the first frame update
     void Start()
     {
         holding = false;
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(0.0f, 0.0f, -speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
-        //Debug.Log(holding);
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 
     public void changeHold(bool state)
