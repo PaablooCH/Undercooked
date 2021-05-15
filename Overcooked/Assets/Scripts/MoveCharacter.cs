079fac57ceca1502d044cad3e6bf8efc8b980ff9
Pablo Cebollada Hernández <pablo_ch@MacBook-Pro-de-Pablo.local>
2021-05-16 00:19:24 +0200

    moveCharacter fixed pick food

--- Overcooked/Assets/Scripts/MoveCharacter.cs
+++ Overcooked/Assets/Scripts/MoveCharacter.cs
@@ -11,14 +11,14 @@ public class MoveCharacter : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
     // Start is called before the first frame update
     void Start()
     {
         holding = false;
-        controller = gameObject.GetComponent<CharacterController>();
+        controller = this.GetComponent<CharacterController>();
+        controller.detectCollisions = false;
     }
 
     // Update is called once per frame
